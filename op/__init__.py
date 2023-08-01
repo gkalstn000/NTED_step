3df63855dcb84a62cc8e5019c6289725f22586f7
gkalstn000 <gkalstn000@gmail.com>
2023-08-01 10:59:01 +0900

    버그 수정 완료 ./cache 파일 새로 생성해야함

--- op/__init__.py
+++ op/__init__.py
@@ -1,2 +1,2 @@
-from .fused_act import FusedLeakyReLU, fused_leaky_relu
-from .upfirdn2d import upfirdn2d
+from op.fused_act import FusedLeakyReLU, fused_leaky_relu
+from op.upfirdn2d import upfirdn2d
